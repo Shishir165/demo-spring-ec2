4bada1397348501b4c3ed10e6ff9e8c418ef3536
Shishir Pariyar <ayudh165@gmail.com>
2026-02-08 19:03:06 +0100

    Initial Spring Boot Project from Spring Initializer

---
+++ src/main/java/com/example/demo_spring_ec2/DemoSpringEc2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo_spring_ec2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringEc2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringEc2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo_spring_ec2/DemoSpringEc2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_spring_ec2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringEc2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

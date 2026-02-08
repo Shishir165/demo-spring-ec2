eb33f28081b927483cd26adb11183d66aa89f8e9
Shishir Pariyar <ayudh165@gmail.com>
2026-02-08 20:21:46 +0100

    Add greet API endpoint

--- src/main/java/com/example/demo_spring_ec2/controller/HelloController.java
+++ src/main/java/com/example/demo_spring_ec2/controller/HelloController.java
@@ -7,13 +7,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+    // Route 1
     @GetMapping("/api/hello")
     public String hello() {
         return "Hello from Spring Boot on EC2!";
     }
 
+    // Route 2
     @GetMapping("/api/greet/{name}")
     public String greet(@PathVariable String name) {
-        return "Hello " + name + ", welcome to our Spring Boot app!";
+        return "Hello " + name + ", welcome to our EC2-hosted Spring Boot app!";
     }
 }
